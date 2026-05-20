4bc4d6c64bc9057d855fd3c1c3dfc04a96c4f5e8
imgifra <francoperezclaro@gmail.com>
2026-05-19 23:55:15 -0500

    add run-once script for github actions

---
+++ scripts/pipeline/run-once.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+
+const { logger } = require("../utils/logger");
+const { runCarouselPipeline } = require("./run-carousel");
+
+async function main() {
+  const cycleId = `${Date.now()}`;
+  logger.info("Ejecutando pipeline una sola vez", { cycleId });
+
+  const result = await runCarouselPipeline({
+    cycleId,
+    branch: "scheduled"
+  });
+
+  if (!result.ok) {
+    logger.error("Pipeline falló", result);
+    process.exit(1);
+  }
+
+  logger.info("Pipeline completado", result);
+  process.exit(0);
+}
+
+main().catch((error) => {
+  logger.error("Error fatal", {}, error);
+  process.exit(1);
+});
