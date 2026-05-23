a3ab894130a037d95bfb49c0c5f44042bab1ee16
imgifra <francoperezclaro@gmail.com>
2026-05-23 01:16:22 -0500

    fix: remover funciones duplicadas en facebook-lib

--- scripts/libs/facebook-lib.js
+++ scripts/libs/facebook-lib.js
@@ -15,30 +15,6 @@ function ensureEnv() {
   }
 }
 
-function buildGraphUrl(path) {
-  return `https://graph.facebook.com/${GRAPH_API_VERSION}/${path}`;
-}
-
-function buildGraphErrorMessage(res, data, rawText) {
-  const error = data?.error;
-
-  if (!error) {
-    return `Graph API error ${res.status}${rawText ? `: ${rawText}` : ""}`;
-  }
-
-  const parts = [
-    error.message || `Graph API error ${res.status}`
-  ];
-
-  if (error.code !== undefined) parts.push(`code=${error.code}`);
-  if (error.error_subcode !== undefined) parts.push(`subcode=${error.error_subcode}`);
-  if (error.error_user_title) parts.push(`title=${error.error_user_title}`);
-  if (error.error_user_msg) parts.push(`user_msg=${error.error_user_msg}`);
-  if (error.fbtrace_id) parts.push(`fbtrace_id=${error.fbtrace_id}`);
-
-  return parts.join(" | ");
-}
-
 async function graphPost(path, body) {
   const url = buildGraphUrl(path);
 
